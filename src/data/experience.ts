7e65bbdfab9cffa947f3f81b41a9a89c950837cf
Albin <alvinfer67@gmail.com>
2025-01-25 17:56:13 -0600

    refactor: using data directory for data

---
+++ src/data/experience.ts
@@ -0,0 +1,14 @@
+export const experience = [
+    {
+      company: "Freelance",
+      activities:
+        "Diseñé e implementé una solución web que optimizó los procesos de atención al cliente, mejorando la eficiencia y reduciendo tiempos de espera, apliqué tecnologías modernas y enfoques ágiles para asegurar un sistema escalable y fácil de mantener en un restaurante de la ciudad de León",
+      date: "Enero 2024 - Junio 2024",
+    },
+    {
+      company: "Freelance",
+      activities:
+        "Diseñé e implementé una herramienta web que permitió a las autoridades universitarias crear, gestionar y analizar encuestas dirigidas a los estudiantes, incorporé un sistema intuitivo para estudiantes y administradores, con funciones avanzadas de reportes y análisis en la universidad UNAN LEÓN.",
+      date: "Agosto 2024 - Noviembre 2024",
+    },
+  ];
---
+++ src/data/proyects.ts
@@ -0,0 +1,15 @@
+import Portfolio from '../../public/portfolio.webp';
+export const proyects  = [
+    {
+      image: Portfolio ,
+      name: "Portfolio",
+      description: "Portafilio desarrollado para mostrar mis habilidades y experiencia como desarrollador de software, utilizando tecnologias modernas y buenas practicas de desarrollo Portafilio desarrollado para mostrar mis habilidades y experiencia como desarrollador de software, utilizando tecnologias modernas y buenas practicas de desarrollo",
+      technologies: ["React", "Astro", "Tailwind", "Typescript","React", "Astro", "Tailwind", "Typescript", "Astro", "Tailwind", "Typescript"]
+    },
+    {
+      image: Portfolio ,
+      name: "Portfolio",
+      description: "Portafilio desarrollado para mostrar mis habilidades y experiencia como desarrollador de software, utilizando tecnologias modernas y buenas practicas de desarrollo Portafilio desarrollado para mostrar mis habilidades y experiencia como desarrollador de software, utilizando tecnologias modernas y buenas practicas de desarrollo",
+      technologies: ["React", "Astro", "Tailwind", "Typescript","React", "Astro", "Tailwind", "Typescript", "Astro", "Tailwind", "Typescript"]
+    }
+  ]
