60d9f2ef11ab7eb6eae5d86cfd5a53af403eecf7
Albin <alvinfer67@gmail.com>
2025-10-22 22:36:15 -0600

    feat(experience): enhance experience card ui

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,14 +1,14 @@
 export const experience = [
     {
-      company: "Freelance",
-      activities:
-        "Diseñé e implementé una solución web que optimizó los procesos de atención al cliente, mejorando la eficiencia y reduciendo tiempos de espera, apliqué tecnologías modernas y enfoques ágiles para asegurar un sistema escalable y fácil de mantener en un restaurante de la ciudad de León",
-      date: "Enero 2024 - Junio 2024",
-    },
-    {
-      company: "Freelance",
-      activities:
-        "Diseñé e implementé una herramienta web que permitió a las autoridades universitarias crear, gestionar y analizar encuestas dirigidas a los estudiantes, incorporé un sistema intuitivo para estudiantes y administradores, con funciones avanzadas de reportes y análisis en la universidad UNAN LEÓN.",
-      date: "Agosto 2024 - Noviembre 2024",
+      company: "UNAN LEON",
+      position: "Desarrollador de Software",
+      activities:[
+        "Participé en el modelado de requerimientos y diseño funcional de nuevas características para los sistemas internos de la UNAN-León",
+        "Desarrollé componentes Frontend con React y funcionalidades backend con PHP, asegurando una integración eficiente entre ambos entornos",
+        "Implementé mejoras de rendimiento, corregí errores en producción y optimicé la experiencia del usuario mediante interfaces responsivas y accesibles"
+      ],
+      date: "Junio 2025 - Presente",
+      logo: "/unan_logo.png"
     },
   ];
+  
