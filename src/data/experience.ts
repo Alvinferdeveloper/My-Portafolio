009cbd658b2cf7bdeae1ccf5bc382ad8541d0495
Albin <alvinfer67@gmail.com>
2025-11-21 17:04:56 -0600

    chore: change experience date and cv version

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,14 +1,13 @@
 export const experience = [
-    {
-      company: "UNAN LEON",
-      position: "Desarrollador de Software",
-      activities:[
-        "Participé en el modelado de requerimientos y diseño funcional de nuevas características para los sistemas internos de la UNAN-León",
-        "Desarrollé componentes Frontend con React y funcionalidades backend con PHP, asegurando una integración eficiente entre ambos entornos",
-        "Implementé mejoras de rendimiento, corregí errores en producción y optimicé la experiencia del usuario mediante interfaces responsivas y accesibles"
-      ],
-      date: "Junio 2025 - Presente",
-      logo: "/unan_logo.png"
-    },
-  ];
-  
+  {
+    company: "UNAN LEON",
+    position: "Desarrollador de Software",
+    activities: [
+      "Participé en el modelado de requerimientos y diseño funcional de nuevas características para los sistemas internos de la UNAN-León",
+      "Desarrollé componentes Frontend con React y funcionalidades backend con PHP, asegurando una integración eficiente entre ambos entornos",
+      "Implementé mejoras de rendimiento, corregí errores en producción y optimicé la experiencia del usuario mediante interfaces responsivas y accesibles"
+    ],
+    date: "Agosto - Noviembre 2025",
+    logo: "/unan_logo.png"
+  },
+];
